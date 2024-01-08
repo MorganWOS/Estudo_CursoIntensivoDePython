d686095185f8d4fe87d9088ba534a9883b218c35
MorganWOS <101461868+MorganWOS@users.noreply.github.com>
2024-01-08 13:12:21 -0300

    Update aprendendo_python.py

--- livro3(cursointensivo)/arquivos_exceções/aprendendo_python.py
+++ livro3(cursointensivo)/arquivos_exceções/aprendendo_python.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-path = Path("/home/morganwos/programação/projetos(python)/livro3(cursointensivo)/arquivos_exceções/learning_python.txt")
+path = Path("learning_python.txt")
 content = path.read_text().strip()
 lines = content.splitlines()
 for line in lines:
